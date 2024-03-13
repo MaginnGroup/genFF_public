23a5a09502a3e459a47c4d86eb53b3570c0be826
Montana Carlozo <mcarlozo@nd.edu>
2024-03-12 20:04:15 -0400

    Added results

--- make_hms.py
+++ make_hms.py
@@ -6,6 +6,7 @@ import unyt as u
 #Set params for saving results and whether obj wts are scaled
 save_data = True
 scl_w = 2
+opt_choice = "SSE"
 
 #Load class properies for each molecule
 r14_class = r14.R14Constants()
@@ -17,11 +18,17 @@ r143a_class = r143a.R143aConstants()
 r170_class = r170.R170Constants()
 
 #Get dict of refrigerant classes to consider, gps, and atom typing class
-molec_data_dict = {"R32":r32_class}
+molec_data_dict = {"R14":r14_class, 
+                   "R32":r32_class, 
+                   "R50":r50_class, 
+                   "R170":r170_class, 
+                   "R125":r125_class, 
+                   "R134a":r134a_class, 
+                   "R143a":r143a_class}
 
-at_class = atom_type.AT_Scheme_7()
+at_class = atom_type.AT_Scheme_9()
 all_gp_dict = opt_atom_types.get_gp_data_from_pkl(list(molec_data_dict.keys()))
 #Best set from Experiment
-best_set = np.array([3.830327452839443,2.9719813341938197,2.156373165481484,2.6941766822509,3.8523628534129073,3.837466871267212,53.22755465997726,72.61206434864629,8.547339075792452,33.29151423562465,19.727770531635596,32.69817579869139])
-visual = opt_atom_types.Vis_Results(molec_data_dict, all_gp_dict, at_class, scl_w, save_data)
+best_set = np.array([3.4651788382627764,3.9218070855238745,3.9999999999999996,1.691292101355337,2.290823592634345,2.948836531411303,3.8698590079226696,50.74628450631082,54.58103944130325,75.00000000000001,10.000000000000002,50.00000000000001,50.00000000000001,50.00000000000001])
+visual = opt_atom_types.Vis_Results(molec_data_dict, all_gp_dict, at_class, scl_w, opt_choice, save_data)
 visual.plot_obj_hms(best_set)
