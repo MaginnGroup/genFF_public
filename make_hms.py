c83f8cba9d9f3547db6bb119e91ea2c6cd04de9e
Montana Carlozo <mcarlozo@nd.edu>
2024-03-15 11:08:55 -0400

    makeing heat maps for at9_ess_full

--- make_hms.py
+++ make_hms.py
@@ -6,7 +6,7 @@ import unyt as u
 #Set params for saving results and whether obj wts are scaled
 save_data = True
 scl_w = 2
-opt_choice = "SSE"
+opt_choice = "ExpVal"
 
 #Load class properies for each molecule
 r14_class = r14.R14Constants()
@@ -29,6 +29,6 @@ molec_data_dict = {"R14":r14_class,
 at_class = atom_type.AT_Scheme_9()
 all_gp_dict = opt_atom_types.get_gp_data_from_pkl(list(molec_data_dict.keys()))
 #Best set from Experiment
-best_set = np.array([3.4651788382627764,3.9218070855238745,3.9999999999999996,1.691292101355337,2.290823592634345,2.948836531411303,3.8698590079226696,50.74628450631082,54.58103944130325,75.00000000000001,10.000000000000002,50.00000000000001,50.00000000000001,50.00000000000001])
+best_set = np.array([3.43222589608333,3.8608505577545786,3.9999999999999996,1.9211526390460762,2.554453505316342,2.8776761377012368,2.997222431322794,45.538857572560964,47.52298488436068,75.00000000000001,10.000000000000002,50.00000000000001,50.00000000000001,50.00000000000001])
 visual = opt_atom_types.Vis_Results(molec_data_dict, all_gp_dict, at_class, scl_w, opt_choice, save_data)
 visual.plot_obj_hms(best_set)
