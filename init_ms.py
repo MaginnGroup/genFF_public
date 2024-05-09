974ce22667c1524efd1369256f44f7b2043f8429
Montana Carlozo <mcarlozo@nd.edu>
2024-05-09 15:03:32 -0400

    Fixed bugs in init_ms.py

--- init_ms.py
+++ init_ms.py
@@ -4,8 +4,6 @@ import unyt as u
 import sys
 import pandas as pd
 
-sys.path.append("../../analysis/")
-from fffit.utils import values_scaled_to_real
 from utils.molec_class_files import r14, r32, r50, r125, r134a, r143a, r170, r41, r23, r161, r152a, r152, r134, r143, r116
 from utils import atom_type, opt_atom_types
 
@@ -44,61 +42,9 @@ molec_dict = {"R14": R14,
                 # "R143": R143,
                 # "R134": R134,
                 "R116": R116}
-          
-def init_project():
 
-    # Initialize project
-    project = signac.init_project("opt_ff_ms")
-
-    #Loop over all molecules
-    for molec_name, molec_data in molec_dict.items():
-        # Define temps (from constants files)
-        temps = list(molec_data.expt_Pvap.keys())
-
-        # Run at vapor pressure (from constants file)
-        press = molec_data.expt_Pvap.values()
-
-        # Load sample from best set using ExpVal and all training molecules
-        at_class = atom_type.make_atom_type_class(at_number)
-        save_data = False
-        obj_choice = "ExpVal"
-        molec_data_dict = {"R14":R14, "R32":R32, "R50":R50, "R170":R170, "R125":R125, "R134a":R134a, "R143a":R143a}
-        all_gp_dict = opt_atom_types.get_gp_data_from_pkl(list(molec_data_dict.keys()))
-        setup = opt_atom_types.Problem_Setup(molec_data_dict, all_gp_dict, at_class, obj_choice)
-        all_molec_dir = setup.make_results_dir(list(molec_data_dict.keys()))
-        all_df = pd.read_csv(all_molec_dir+"/best_per_run.csv", header = 0)
-        first_param_name = setup.at_class.at_names[0] + "_min"
-        last_param_name = setup.at_class.at_names[-1] + "_min"
-        full_opt_best = all_df.loc[0, first_param_name:last_param_name]
-
-        # Convert to units of nm and kJ/mol
-        param_matrix = at_class.get_transformation_matrix({molec_name:molec_data})
-        all_best_real = setup.values_pref_to_real(full_opt_best.values)
-        #Parmaeters in units nm and kj/mol
-        scaled_params = all_best_real.reshape(-1,1).T@param_matrix
-
-        for temp in temps:
-            #Theoretically, we could examine more than just the best
-            for sample in scaled_params:
-                # Define the initial state point
-                state_point = {
-                    "atom_type": at_number,
-                    "mol_name": molec_name,
-                    "mol_weight": molec_data.molecular_weight.in_units(u.amu).value,
-                    "smiles": molec_data.smiles_str,
-                    "N_atoms": molec_data.n_atoms,
-                    "T": float(temp.in_units(u.K).value),
-                    "P": float(press[int(temp.in_units(u.K).value)].in_units(u.bar).value),
-                    "N_vap": n_vap,
-                    "N_liq": n_liq,
-                    "expt_liq_density": molec_data.expt_liq_density[int(temp.in_units(u.K).value)],
-                    "nsteps_liqeq": 5000,
-                    "nsteps_eq": 10000,
-                    "nsteps_prod": 100000,
-                }
-                state_point = unpack_molec_values(molec_name, at_class, sample, state_point)            
-                job = project.open_job(state_point)
-                job.init()
+# Initialize project
+project = signac.init_project("opt_ff_ms")
 
 def unpack_molec_values(molec_name, at_class, sample, state_point):
     """
@@ -107,8 +53,63 @@ def unpack_molec_values(molec_name, at_class, sample, state_point):
     # Unpack the sample according to atom typing scheme mapping dictionary
     molec_map_dict = at_class.molec_map_dicts[molec_name]
     param_names = molec_map_dict.keys()
+    #Get param names in order of original mapping
+    order = molec_data.param_names
+    index_mapping = {elem: idx for idx, elem in enumerate(order)}
     #Add params based on the order they show up in given the mapping
-    for i, param in enumerate(param_names):
-        state_point[param] = sample[molec_map_dict[i]]
+    for param in param_names:
+        state_point[param] = sample[index_mapping[param]]
 
     return state_point
+
+#Loop over all molecules
+for molec_name, molec_data in molec_dict.items():
+    # Define temps (from constants files)
+    temps = list(molec_data.expt_Pvap.keys())
+
+    # Run at vapor pressure (from constants file)
+    press = molec_data.expt_Pvap
+
+    # Load sample from best set using ExpVal and all training molecules
+    at_class = atom_type.make_atom_type_class(at_number)
+    save_data = False
+    obj_choice = "ExpVal"
+    molec_data_dict = {"R14":R14, "R32":R32, "R50":R50, "R170":R170, "R125":R125, "R134a":R134a, "R143a":R143a}
+    all_gp_dict = opt_atom_types.get_gp_data_from_pkl(list(molec_data_dict.keys()))
+    setup = opt_atom_types.Problem_Setup(molec_data_dict, all_gp_dict, at_class, obj_choice)
+    all_molec_dir = setup.make_results_dir(list(molec_data_dict.keys()))
+    all_df = pd.read_csv(all_molec_dir+"/best_per_run.csv", header = 0)
+    first_param_name = setup.at_class.at_names[0] + "_min"
+    last_param_name = setup.at_class.at_names[-1] + "_min"
+    full_opt_best = all_df.loc[0, first_param_name:last_param_name]
+
+    # Convert to units of nm and kJ/mol
+    param_matrix = at_class.get_transformation_matrix({molec_name:molec_data})
+    all_best_real = setup.values_pref_to_real(full_opt_best.values)
+    #Parmaeters in units nm and kJ/mol
+    scaled_params = all_best_real.reshape(-1,1).T@param_matrix
+
+    for temp in temps:
+        #Theoretically, we could examine more than just the best
+        for sample in scaled_params:
+            # Define the initial state point
+            state_point = {
+                "atom_type": at_number,
+                "mol_name": molec_name,
+                "mol_weight": molec_data.molecular_weight, #amu
+                "smiles": molec_data.smiles_str,
+                "N_atoms": molec_data.n_atoms,
+                "T": float(temp), #K
+                "P": float(press[int(temp)]), #bar
+                "N_vap": n_vap,
+                "N_liq": n_liq,
+                "expt_liq_density": molec_data.expt_liq_density[int(temp)], #kg/m^3
+                "nsteps_liqeq": 5000,
+                "nsteps_eq": 10000,
+                "nsteps_prod": 100000,
+            }
+            state_point = unpack_molec_values(molec_name, at_class, sample, state_point)       
+            job = project.open_job(state_point)
+            job.init()
+
+
