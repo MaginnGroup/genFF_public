0c35f4470d5b92af5722f47953a1686d25170be1
Montana Carlozo <mcarlozo@nd.edu>
2024-07-03 08:36:39 -0400

    Added pareto info for ExpValPrior at11 and updated post_analysis script

--- post_analysis_opt.py
+++ post_analysis_opt.py
@@ -13,7 +13,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 #Set params for what you want to analyze
 save_data = True #Data to save
-obj_choice = "ExpVal" #Objective to consider
+obj_choice = "ExpValPrior" #Objective to consider
 at_number = 11 #atom type to consider
 seed = 1 #Seed to use
 molec_names = ["R14", "R32", "R50", "R170", "R125", "R134a", "R143a"] #Training data to consider
@@ -23,20 +23,24 @@ project = signac.get_project("opt_at_params")
 filtered_jobs = project.find_jobs({"obj_choice": obj_choice, "atom_type": at_number})
 grouped_jobs = filtered_jobs.groupby(statepoint="training_molecules")
 for statepoint_value, group in grouped_jobs.items():
-    #Create dataframe of all results in job.fn("best_run.csv")
-    unsorted_df = pd.DataFrame()
+    #For each group of training molecules, get the first job to get the path to the directory
+    jobs_in_group = list(grouped_jobs[group])
+    save_path = jobs_in_group[0].document.dir_name
     for i,job in enumerate(group):
-        #On the 1st iteration, save the path to the job directory
-        if i == 0:
-            save_path = job.document.dir_name
+        #If the best run file exists
         if os.path.exists(job.fn("best_run.csv")):
+            #Read the file and concatenate the data
             df_best_run = pd.read_csv(job.fn("best_run.csv"), header = 0, index_col=False)
-            all_df = pd.concat([all_df, df_best_run], ignore_index=True)
-        else:
-            pass
+            #On the 1st iteration, create the df
+            if i ==0:
+                unsorted_df = df_best_run
+            #Otherwise append to it
+            else:
+                unsorted_df = pd.concat([unsorted_df, df_best_run], ignore_index=True)
+    #Sort the data by the minimum objective value        
     all_df = unsorted_df.sort_values(by='Min Obj', ascending = True).reset_index(drop = True)
     #Save all the best sets in appropriate folder for each set of training molecules
-    all_df.to_csv(os.path.join(save_path, "best_per_run.csv"), index=False)
+    all_df.to_csv(os.path.join(save_path, "best_per_run.csv"), index=False, header=True)
 
 #Create visualization object
 visual = opt_atom_types.Vis_Results(molec_names, at_number, seed, obj_choice)
@@ -58,6 +62,9 @@ visual.comp_paper_full_ind(molec_names_all, save_label=x_label)
 #Calculate MAPD for predictions and save results
 df = visual.calc_MAPD_best(molec_names_all, save_data, save_label=x_label)
 
+#Plot optimization result heat maps
+visual.plot_obj_hms(best_set, x_label)
+
 #Gat Jac and Hess Approximations
 scale_theta = True
 jac = visual.approx_jac(best_real, scale_theta, save_data, x_label=x_label)
@@ -70,17 +77,14 @@ if save_data == True:
     np.savetxt(eig_val_path,eigval,delimiter=",")
     np.savetxt(eig_vec_path,eigvec,delimiter=",")
 
-#Plot optimization result heat maps
-visual.plot_obj_hms(best_set, x_label)
-
 #Plot atom_type scheme results
-at_schemes = [11,12,13,14]
-if len(at_schemes) > 1 and isinstance(at_schemes, (list,np.ndarray)):
-    at_str = '-'.join(at_schemes.sort())
-else:
-    at_str = at_schemes[0]
-pdf = PdfPages('Results/at_schemes_' + at_str + '.pdf')
-pdf.savefig(visual.plot_at_MSE(molec_names, at_schemes), bbox_inches='tight')
-#Close figures 
-plt.close()
-pdf.close()   
+# at_schemes = [11,12,13,14]
+# if len(at_schemes) > 1 and isinstance(at_schemes, (list,np.ndarray)):
+#     at_str = '-'.join(at_schemes.sort())
+# else:
+#     at_str = at_schemes[0]
+# pdf = PdfPages('Results/at_schemes_' + at_str + '.pdf')
+# pdf.savefig(visual.plot_at_MSE(molec_names, at_schemes), bbox_inches='tight')
+# #Close figures 
+# plt.close()
+# pdf.close()   
