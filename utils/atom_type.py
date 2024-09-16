bd7ed21f945fd895501b24d64657c3b1b6d32215
Montana Carlozo <mcarlozo@nd.edu>
2024-09-16 16:58:57 -0400

    Generated atom types for GAFF and CFH

--- utils/atom_type.py
+++ utils/atom_type.py
@@ -137,6 +137,380 @@ class Atom_Types:
 
         return len(tuple_list) != len(set(tuple_list))
     
+class AT_Scheme_01(Atom_Types):
+    """
+    Class for Atom Type Scheme 1 (C,F,H)
+
+    Methods
+    -------
+    __init__(self)
+    """
+    def __init__(self):
+        #Get Bounds
+        at_param_bounds_l = [2, 1.5, 2, 10, 2, 15] #Units of Angstroms and Kelvin for Sigmas and Epsilons
+        at_param_bounds_u = [4, 3, 4, 75, 15, 50]
+        at_bounds = np.array([at_param_bounds_l, at_param_bounds_u]).T
+        self.scheme_name = "at_01"
+        #Get Names
+        at_keys = ["sigma_C", "sigma_H", "sigma_F", "epsilon_C", "epsilon_H", "epsilon_F"]
+        assert len(at_keys) == len(at_param_bounds_l) == len(at_param_bounds_u), "Length of at_keys, at_param_bounds_l, and at_param_bounds_u must be the same"
+        # Get weight information
+        # at_weights = np.zeros(len(at_keys))
+        # gaff_params = np.array(at_param_bounds_l) #set initial gaff parameters as lower bounds
+        # mask_names = list(["sigma_C2_2", "sigma_F_1", "epsilon_C1", "epsilon_C2_1", "epsilon_F_1"])
+        # mask = np.isin(at_keys, mask_names)
+        # GAFF_params = np.array([3.400, 3.118, 55.052, 55.052, 30.696])
+        # #For all of these, a value of 0.15 difference from GAFF is weighted as 5% of the average best objective for ExpVal
+        # g_weights = np.array([0.05/0.15**2, 0.05/0.15**2, 0.2/0.15**2, 0.05/0.15**2, 0.05/0.15**2])
+        # at_weights[mask] = g_weights
+        # gaff_params[mask] = GAFF_params
+        # self.weighted_params = mask_names
+        # self.at_weights = at_weights
+        # self.gaff_params = gaff_params
+
+        #Create a file that maps param names (keys) to at_param names for atom type 11 (values) for each molecule
+        r14_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_F1": "epsilon_F"}
+
+        r32_map_dict = {"sigma_C": "sigma_C",
+                        "sigma_H": "sigma_H",
+                        "sigma_F": "sigma_F",
+                        "epsilon_C": "epsilon_C",
+                        "epsilon_H": "epsilon_H",
+                        "epsilon_F": "epsilon_F"
+                        }
+
+        r50_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H"}
+
+        r125_map_dict = {"sigma_C1":"sigma_C",
+                    "sigma_C2": "sigma_C",
+                    "sigma_H1": "sigma_H",
+                    "sigma_F1":"sigma_F",
+                    "sigma_F2": "sigma_F",
+                    "epsilon_C1": "epsilon_C",
+                    "epsilon_C2": "epsilon_C",
+                    "epsilon_H1": "epsilon_H",
+                    "epsilon_F1": "epsilon_F",
+                    "epsilon_F2": "epsilon_F"
+                    }
+
+        r134a_map_dict = {"sigma_C2": "sigma_C",
+                    "sigma_C1": "sigma_C",
+                    "sigma_H1": "sigma_H",
+                    "sigma_F2": "sigma_F",
+                    "sigma_F1": "sigma_F",
+                    "epsilon_C2": "epsilon_C",
+                    "epsilon_C1": "epsilon_C",
+                    "epsilon_H1":"epsilon_H",
+                    "epsilon_F2": "epsilon_F",
+                    "epsilon_F1": "epsilon_F"
+                    }
+
+        r143a_map_dict = {"sigma_C2": "sigma_C",
+                          "sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_F1": "epsilon_F",
+                        }
+
+        r170_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H"}
+
+        #Test molecules
+        r41_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_F1": "epsilon_F"
+                        } 
+
+        r23_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_F1": "epsilon_F"
+                        } 
+        
+        r161_map_dict = {"sigma_C1": "sigma_C",
+                         "sigma_C2": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_H2": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_H2": "epsilon_H",
+                        "epsilon_F1": "epsilon_F",
+                        }
+        
+        r152a_map_dict = {"sigma_C1": "sigma_C",
+                         "sigma_C2": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_H2": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_H2": "epsilon_H",
+                        "epsilon_F1": "epsilon_F",
+                        }
+        
+        r152_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_F1": "epsilon_F",
+                        }
+
+        r143_map_dict = {"sigma_C2": "sigma_C",
+                         "sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H",
+                        "sigma_H2": "sigma_H",
+                        "sigma_F2": "sigma_F",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H",
+                        "epsilon_H2": "epsilon_H",
+                        "epsilon_F2": "epsilon_F",
+                        "epsilon_F1": "epsilon_F",
+                        }
+        
+        r134_map_dict = {"sigma_C": "sigma_C",
+                        "sigma_H": "sigma_H",
+                        "sigma_F": "sigma_F",
+                        "epsilon_C": "epsilon_C",
+                        "epsilon_H": "epsilon_H",
+                        "epsilon_F": "epsilon_F"
+                        } 
+        
+        r116_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_F1": "epsilon_F"}
+        
+        at_names = at_keys.copy()
+        
+        molec_map_dicts = {"R14":r14_map_dict,
+            "R32":r32_map_dict,
+            "R50":r50_map_dict,
+            "R125":r125_map_dict,
+            "R134a":r134a_map_dict,
+            "R143a":r143a_map_dict,
+            "R170":r170_map_dict,
+            "R41":r41_map_dict,
+            "R23":r23_map_dict,
+            "R161":r161_map_dict,
+            "R152a":r152a_map_dict,
+            "R152":r152_map_dict,
+            "R143":r143_map_dict,
+            "R134":r134_map_dict,
+            "R116":r116_map_dict}
+        
+        super().__init__(at_bounds, at_names, molec_map_dicts)
+        #Get scaled bounds
+        self.scale_bounds()
+
+class AT_Scheme_02(Atom_Types):
+    """
+    Class for Atom Type Scheme 2 (GAFF)
+
+    Methods
+    -------
+    __init__(self)
+    """
+    def __init__(self):
+        #Get Bounds
+        at_param_bounds_l = [2, 1.5, 1.5, 1.5, 1.5, 2, 10, 2, 2, 2, 2, 15] #Units of Angstroms and Kelvin for Sigmas and Epsilons
+        at_param_bounds_u = [4, 3,   3,   3,   3,   4, 75, 15,15,15,15, 50]
+        at_bounds = np.array([at_param_bounds_l, at_param_bounds_u]).T
+        self.scheme_name = "at_02"
+        #Get Names
+        at_keys = ["sigma_C", "sigma_Hc", "sigma_H1", "sigma_H2", "sigma_H3", "sigma_F", 
+                   "epsilon_C", "epsilon_Hc", "epsilon_H1", "epsilon_H2","epsilon_H3", "epsilon_F"]
+        assert len(at_keys) == len(at_param_bounds_l) == len(at_param_bounds_u), "Length of at_keys, at_param_bounds_l, and at_param_bounds_u must be the same"
+        # Get weight information
+        # at_weights = np.zeros(len(at_keys))
+        # gaff_params = np.array(at_param_bounds_l) #set initial gaff parameters as lower bounds
+        # mask_names = list(["sigma_C2_2", "sigma_F_1", "epsilon_C1", "epsilon_C2_1", "epsilon_F_1"])
+        # mask = np.isin(at_keys, mask_names)
+        # GAFF_params = np.array([3.400, 3.118, 55.052, 55.052, 30.696])
+        # #For all of these, a value of 0.15 difference from GAFF is weighted as 5% of the average best objective for ExpVal
+        # g_weights = np.array([0.05/0.15**2, 0.05/0.15**2, 0.2/0.15**2, 0.05/0.15**2, 0.05/0.15**2])
+        # at_weights[mask] = g_weights
+        # gaff_params[mask] = GAFF_params
+        # self.weighted_params = mask_names
+        # self.at_weights = at_weights
+        # self.gaff_params = gaff_params
+
+        #Create a file that maps param names (keys) to at_param names for atom type 11 (values) for each molecule
+        r14_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_F1": "epsilon_F"}
+
+        r32_map_dict = {"sigma_C": "sigma_C",
+                        "sigma_H": "sigma_H2",
+                        "sigma_F": "sigma_F",
+                        "epsilon_C": "epsilon_C",
+                        "epsilon_H": "epsilon_H2",
+                        "epsilon_F": "epsilon_F"
+                        }
+
+        r50_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_Hc",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_Hc"}
+
+        r125_map_dict = {"sigma_C1":"sigma_C",
+                    "sigma_C2": "sigma_C",
+                    "sigma_H1": "sigma_H2",
+                    "sigma_F1":"sigma_F",
+                    "sigma_F2": "sigma_F",
+                    "epsilon_C1": "epsilon_C",
+                    "epsilon_C2": "epsilon_C",
+                    "epsilon_H1": "epsilon_H2",
+                    "epsilon_F1": "epsilon_F",
+                    "epsilon_F2": "epsilon_F"
+                    }
+
+        r134a_map_dict = {"sigma_C1": "sigma_C",
+                    "sigma_C2": "sigma_C",
+                    "sigma_H1": "sigma_H1",
+                    "sigma_F1": "sigma_F",
+                    "sigma_F2": "sigma_F",
+                    "epsilon_C1": "epsilon_C",
+                    "epsilon_C2": "epsilon_C",
+                    "epsilon_H1":"epsilon_H1",
+                    "epsilon_F1": "epsilon_F",
+                    "epsilon_F2": "epsilon_F"
+                    }
+
+        r143a_map_dict = {"sigma_C1": "sigma_C",
+                          "sigma_C2": "sigma_C",
+                        "sigma_H1": "sigma_Hc",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H1": "epsilon_Hc",
+                        "epsilon_F1": "epsilon_F",
+                        }
+
+        r170_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_Hc",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_Hc"}
+
+        #Test molecules
+        r41_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H1",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H1",
+                        "epsilon_F1": "epsilon_F"
+                        } 
+
+        r23_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H3",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H3",
+                        "epsilon_F1": "epsilon_F"
+                        } 
+        
+        r161_map_dict = {"sigma_C1": "sigma_C",
+                         "sigma_C2": "sigma_C",
+                         "sigma_H2": "sigma_Hc",
+                        "sigma_H1": "sigma_H1",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H2": "epsilon_Hc",
+                        "epsilon_H1": "epsilon_H1",
+                        "epsilon_F1": "epsilon_F",
+                        }
+        
+        r152a_map_dict = {"sigma_C1": "sigma_C",
+                         "sigma_C2": "sigma_C",
+                        "sigma_H1": "sigma_Hc",
+                        "sigma_H2": "sigma_H2",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H1": "epsilon_Hc",
+                        "epsilon_H2": "epsilon_H2",
+                        "epsilon_F1": "epsilon_F",
+                        }
+        
+        r152_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_H1": "sigma_H1",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_H1": "epsilon_H1",
+                        "epsilon_F1": "epsilon_F",
+                        }
+
+        r143_map_dict = {"sigma_C1": "sigma_C",
+                         "sigma_C2": "sigma_C",
+                         "sigma_H2": "sigma_H1",
+                        "sigma_H1": "sigma_H2",
+                        "sigma_F1": "sigma_F",
+                        "sigma_F2": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_C2": "epsilon_C",
+                        "epsilon_H2": "epsilon_H1",
+                        "epsilon_H1": "epsilon_H2",
+                        "epsilon_F1": "epsilon_F",
+                        "epsilon_F2": "epsilon_F",
+                        }
+        
+        r134_map_dict = {"sigma_C": "sigma_C",
+                        "sigma_H": "sigma_H2",
+                        "sigma_F": "sigma_F",
+                        "epsilon_C": "epsilon_C",
+                        "epsilon_H": "epsilon_H2",
+                        "epsilon_F": "epsilon_F"
+                        } 
+        
+        r116_map_dict = {"sigma_C1": "sigma_C",
+                        "sigma_F1": "sigma_F",
+                        "epsilon_C1": "epsilon_C",
+                        "epsilon_F1": "epsilon_F"}
+        
+        at_names = at_keys.copy()
+        
+        molec_map_dicts = {"R14":r14_map_dict,
+            "R32":r32_map_dict,
+            "R50":r50_map_dict,
+            "R125":r125_map_dict,
+            "R134a":r134a_map_dict,
+            "R143a":r143a_map_dict,
+            "R170":r170_map_dict,
+            "R41":r41_map_dict,
+            "R23":r23_map_dict,
+            "R161":r161_map_dict,
+            "R152a":r152a_map_dict,
+            "R152":r152_map_dict,
+            "R143":r143_map_dict,
+            "R134":r134_map_dict,
+            "R116":r116_map_dict}
+        
+        super().__init__(at_bounds, at_names, molec_map_dicts)
+        #Get scaled bounds
+        self.scale_bounds()
 
 class AT_Scheme_7(Atom_Types):
     """
