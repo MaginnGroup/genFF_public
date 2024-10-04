9b7badc266f4320c7ceda42ecda39f23f87ad9af
Montana Carlozo <mcarlozo@nd.edu>
2024-10-04 16:29:10 -0400

    Added prelim at8 Ms results and estimability rankings

--- opt_ff_ms/delete_jobs.py
+++ opt_ff_ms/delete_jobs.py
@@ -4,9 +4,12 @@ import signac
 project = signac.get_project()
 
 # Iterate through all jobs in the project
-for job in project:
-    if "restart" in job.sp.keys():
-        if job.sp.restart > 1:
-            print(job.id)
-            # del job.doc["use_crit"]
-            job.remove()
+for job in project.find_jobs({'atom_type': 8}):
+    if job.sp.mol_name == "R152" or job.sp.mol_name == "R134":
+        print(job.sp.T, job.id)
+        job.remove()
+    # if "restart" in job.sp.keys():
+    #     if job.sp.restart > 1:
+    #         print(job.id)
+    #         # del job.doc["use_crit"]
+    #         job.remove()
